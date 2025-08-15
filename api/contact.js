93fe468178eebd8325f182c43e35ba7f245aeeaf
Daniel Deaconescu <daniel.deaconescu98@gmail.com>
2025-08-15 21:23:10 +0300

    further changes

--- api/contact.js
+++ api/contact.js
@@ -45,7 +45,7 @@ export default async (req, res) => {
       secure: true,
       auth: {
         user: process.env.SMTP_USER,
-        pass: process.env.SMPT_PASS,
+        pass: process.env.SMTP_PASS,
       },
       connectionTimeout: 5000,
     });
